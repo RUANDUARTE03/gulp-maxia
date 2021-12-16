9de10ed8884bbbe0f7d3a3f2b03fb633df0835a3
ruanduarte03 <ruanduarte0300@gmail.com>
2021-12-16 01:35:21 -0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const gulp = require("gulp");
+var sass = require("gulp-sass")(require("sass"));
+
+sass.compiler = require("node-sass"); //Necessário para funcionar gulp-sass
+
+const bundleSass = () => {
+  return gulp
+    .src("./static/**/*.scss")
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError)) // Converte Sass para CSS mimificado com gulp-sass
+    .pipe(gulp.dest("./dist"));
+};
+
+exports.bundle = bundleSass;
